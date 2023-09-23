de6f48d9e44a661eb6c2482418bd7ceaaaa489f8
fadilsid <fadilsiddique@gmail.com>
2023-09-23 13:35:53 +0000

    feat: create doctype company

---
+++ leave_management/leave_management/doctype/company/company.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Fadil Siddique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Company', {
+	// refresh: function(frm) {
+
+	// }
+});
