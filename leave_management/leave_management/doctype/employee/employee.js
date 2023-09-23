790035d0c6c326dfb3eee98038249ab0e0947dcd
fadilsid <fadilsiddique@gmail.com>
2023-09-23 13:38:44 +0000

    fix: auto full name

--- leave_management/leave_management/doctype/employee/employee.js
+++ leave_management/leave_management/doctype/employee/employee.js
@@ -5,4 +5,46 @@ frappe.ui.form.on('Employee', {
 	// refresh: function(frm) {
 
 	// }
+
+	before_save: function(frm){
+
+		let first_name = frm.doc.first_name
+		let last_name = frm.doc.last_name
+		let middle_name = frm.doc.middle_name
+
+		let full_name = ''
+
+		if (!middle_name && !last_name){
+			full_name = first_name
+
+			frm.set_value('full_name', full_name)
+		}
+
+		if(middle_name && !last_name){
+			full_name = first_name.concat(" ",middle_name)
+
+			frm.set_value('full_name', full_name)
+
+		}
+
+		if(!middle_name && last_name){
+			full_name = first_name.concat(" ",last_name)
+
+			frm.set_value('full_name', full_name)
+
+		}
+
+		if (middle_name && last_name){
+			full_name = first_name.concat(" ",middle_name," ",last_name)
+
+			frm.set_value('full_name', full_name)
+
+		}
+
+		console.log(full_name)
+
+
+
+
+	}
 });
