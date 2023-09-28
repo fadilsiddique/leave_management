74045fde26e812be7be135e49c873486dfbdb235
fadilsid <fadilsiddique@gmail.com>
2023-09-28 10:46:04 +0000

    fix: leave request validation

--- leave_management/leave_management/doctype/leave_request/leave_request.py
+++ leave_management/leave_management/doctype/leave_request/leave_request.py
@@ -1,8 +1,47 @@
 # Copyright (c) 2023, Fadil Siddique and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class LeaveRequest(Document):
-	pass
+
+	def validate(self):
+		
+		roles = frappe.get_roles(frappe.session.user)
+
+		if 'Employee' in roles:
+			employee_doc=frappe.get_doc('Employee',self.employee)
+			if self.request_type == 'Leave':
+
+				if employee_doc.current_month_leave_balance == 0.0:
+					frappe.throw('Maximum leave for this month is taken. Please contact HR for new request')
+
+			elif self.request_type == 'Excuse':
+			
+				if employee_doc.current_month_excuse_balance == 0.0:
+					frappe.throw('Maximum excuse for this month is taken. Please contact HR for new request')
+
+
+	def on_submit(self):
+
+		if self.leave_status=='Approved':
+
+			employee_doc=frappe.get_doc('Employee',self.employee)
+
+			if self.request_type == 'Leave':
+				if employee_doc.current_month_leave_balance >0.0:
+					employee_doc.db_set('current_month_leave_balance',employee_doc.current_month_leave_balance - 1)
+					
+
+			elif self.request_type == 'Excuse':
+				if employee_doc.current_month_leave_balance >0.0:
+					employee_doc.db_set('current_month_excuse_balance',employee_doc.current_month_excuse_balance - 1)
+
+
+
+				
+			
+			
+
+
