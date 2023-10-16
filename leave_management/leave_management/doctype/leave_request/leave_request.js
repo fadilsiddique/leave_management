bd537814a2d16210a6ccdd4e52715389c95ce4ac
fadilsid <fadilsiddique@gmail.com>
2023-10-16 12:16:43 +0000

    feat: restrict leave and excuse on friday amd saturday

--- leave_management/leave_management/doctype/leave_request/leave_request.js
+++ leave_management/leave_management/doctype/leave_request/leave_request.js
@@ -8,5 +8,28 @@ frappe.ui.form.on('Leave Request', {
 
 	validate: function(frm){
 		
+
+		if ( frappe.user.has_role("Employee")){
+		console.log(frm.doc.request_type, frm.doc.excuse_type)
+
+			if (frm.doc.request_type == 'Leave'){
+				console.log("brororor")
+				let selected_date = new Date(frm.doc.from_date);
+				let day = selected_date.getDay();
+				if (day === 5 || day === 6){
+					frappe.throw('You Cannot Apply For Leave On Friday And Staurday. Please Contact HR.');
+				}
+		}
+
+		if (frm.doc.request_type =='Excuse' && frm.doc.excuse_type == 'Evening'){
+			let excuse_date = new Date(frm.doc.time);
+			let excuse_day = excuse_date.getDay();
+			if (excuse_day === 5 || excuse_day === 6){
+				frappe.throw('You Cannot Apply For Evening Excuse On Friday And Staurday. Please Contact HR.');
+			}
+		}
+
+		}
+		
 	}
 });
