6ffc04fd2a502a9dbd3f8423539096690f5513a2
fadilsid <fadilsiddique@gmail.com>
2023-10-22 12:57:12 +0000

    fix: employee id reset issue

--- leave_management/leave_management/doctype/employee/employee.py
+++ leave_management/leave_management/doctype/employee/employee.py
@@ -4,7 +4,6 @@
 import frappe
 from frappe.model.document import Document
 from leave_management.reset import get_next_month_sunday_count
-from frappe.model.naming import getseries
 
 class Employee(Document):
 	
@@ -32,13 +31,13 @@ class Employee(Document):
 		else:
 			frappe.throw('Enter user linked to current employee in user details')
 
-		# emp_list = frappe.get_list('Employee',fields=['employee_id'])
-		# employee_ids = [item["employee_id"] for item in emp_list]
-		# max_employee_id = max(employee_ids)
+		emp_list = frappe.get_list('Employee',fields=['employee_id'])
+		employee_ids = [item["employee_id"] for item in emp_list]
+		max_employee_id = max(employee_ids)
 
-		# print(max_employee_id)
+		self.employee_id = max_employee_id + 1
 
-		# self.employee_id = max_employee_id + 1
+		self.save()
 
 
 
