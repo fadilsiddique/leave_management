c56ef88a92161a08441ddab39621472e824ca39f
fadilsid <fadilsiddique@gmail.com>
2023-12-24 15:00:18 +0000

    feat: long leave request

---
+++ leave_management/leave_management/doctype/long_leave_request/long_leave_request.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, Fadil Siddique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Long Leave Request', {
+	// refresh: function(frm) {
+
+	// }
+
+	to_date: function(frm){
+
+		console.log(typeof(frm.doc.from_date))
+		
+	}
+
+	
+});
