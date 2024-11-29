fe6d3d5fe501a34c8f8c1a92a5c1f6fce494ab68
fadilsid <fadilsiddique@gmail.com>
2024-11-29 12:19:51 +0000

    fix: manual enter emp id

--- leave_management/leave_management/doctype/employee/employee.py
+++ leave_management/leave_management/doctype/employee/employee.py
@@ -31,13 +31,13 @@ class Employee(Document):
 		else:
 			frappe.throw('Enter user linked to current employee in user details')
 
-		emp_list = frappe.get_list('Employee',fields=['employee_id'])
-		employee_ids = [item["employee_id"] for item in emp_list]
-		max_employee_id = max(employee_ids)
+		# emp_list = frappe.get_list('Employee',fields=['employee_id'])
+		# employee_ids = [item["employee_id"] for item in emp_list]
+		# max_employee_id = max(employee_ids)
 
-		self.employee_id = max_employee_id + 1
+		# self.employee_id = max_employee_id + 1
 
-		self.save()
+		# self.save()
 
 	def on_update(self):
 
